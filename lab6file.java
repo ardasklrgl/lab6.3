fdd50f5eaf489b7f77b34491038c9affead98626
Arda Şıklaroğlu <ardasiklaroglu@Arda-MacBook-Pro.local>
2025-03-21 12:31:36 +0000

    4

--- lab6file.java
+++ lab6file.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hasdfghjjrld");
+    System.out.println("Hasdfghjjld");
+    System.out.println("Heasdfghjjd");
+    System.out.println("asdfghjj");
+    System.out.println("asdfghjj");
+
   }
 }
